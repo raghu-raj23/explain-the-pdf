36e4b222879a73cdabc074eaf2730e324115dc0f
raghu-raj23 <raghuraj.muni@gmail.com>
2024-05-05 23:34:06 +0530

    setup of home page, sign in and sign up page, middleware, file upload component and db schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,44 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    
-    <h1>The app works!!</h1>
-  );
+import { SignUp, UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import Link from "next/link";
+import { LogIn } from "lucide-react";
+import FileUpload from "@/components/FileUpload";
+
+export default async function Home() {
+	const { userId } = await auth();
+	const isAuth = !!userId;
+	return (
+		<div className="m-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+			<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+				<div className="flex flex-col items-center text-center">
+					<div className="flex items-center">
+						<h1 className="mr-3 text-5xl font-semibold">Explain the PDF</h1>
+						<UserButton afterSignOutUrl="/" />
+					</div>
+					<div className="flex mt-2">
+						{isAuth && <Button>Go to Chats</Button>}
+					</div>
+					<p className="max-w-xl mt-1 text-lg text-slate-600">
+						Join millions of professionals, students and researchers to
+						instantly asnwer the questions and researh with GenAI
+					</p>
+					<div className="w-full mt-4">
+						{isAuth ? (
+							<FileUpload/>
+						) : (
+							<Link href={"/sign-in"}>
+								<Button>
+									Log in to get started!
+									<LogIn className="w-4 h-4 ml-2" />
+								</Button>
+							</Link>
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
